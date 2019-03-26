648223d59a2b7c8df55485b4e6f0fac2427ca56a
Nikola Kozhuharov <nikolakozhuharovv@gmail.com>
2019-03-26 11:24:43 -0400

    Documentation and minor upgrades fro Core class

--- core/core.php
+++ core/core.php
@@ -7,16 +7,19 @@ class Core{
      * An instance of the Database class
      */
     public $db;
+    
     /**
      * @var mc
      * An instance of the Memcache class
      */
     public $mc;
+    
     /**
      * @var string
      * The name of the database
      */
     public $dbName;
+    
     /**
      * @var array
      * Contains all the subfolders of the main classes folder
@@ -28,32 +31,38 @@ class Core{
      * Contains the classes inheritance description
      */
     private $classesRequriementTree = array();
+    
     /**
      * @var string
      * The main directory with the project files
      */
     public $projectDir;
+    
     /**
      * @var string
      * The directory with the controllers
      */
     public $controllersDir;
+    
     /**
      * @var string
      * The directory with the views
      */
     public $viewsDir;
+    
     /**
      * @var string
      * The timezone of the project
      * For a list of supported timezones - http://php.net/manual/en/timezones.php
      */
     public $timezone = 'Europe/Sofia';
+    
     /**
      * @var int
      * Query cache time; set to -1 for recache
      */
     public $cacheTime = 0;
+    
     /**
      * @var bool
      * Shows if the request is ajax
@@ -99,6 +108,9 @@ class Core{
     
     public $generalErrorText = 'Sorry There has been some kind of an error with your request. If this persists please Contact Us.';
     
+    /**
+     * A list of IPs, considered as developer IPs
+     */
     public $debugIps = array();
     
     //API VARIABLES
@@ -117,48 +129,64 @@ class Core{
      */         
     public $APIRequiredFiles = array(); 
     
-    //END OF PLATFORM VARIABLES
-
+    /**
+     * Creates an instance of the Core class
+     * Throws Exception if info does not contain 'db' or 'mc'
+     * @param array $info - the configuration array for the platfrom
+     * @param array $classesTree - the decsription of classes inheritane for the project (optional)
+     * @throws Exception
+     */
     public function __construct(array $info, array $classesTree = null){
-        //catch if request is ajax
+        if (!isset($info['db']) || empty($info['db']) || !isset($info['mc']) || empty($info['mc'])) {
+            throw new Exception ("'db' and 'mc' variables are required for a Core instance!");
+        }
+        
         $this->ajax = (
             (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') ||
             (isset($_REQUEST['ajax'])) ||
             (isset($AJAX) && $AJAX == true)
         ) ? true : false;
-
+        
+        //require all Core classes
         foreach (glob(GLOBAL_PATH.'platform/classes/*.php') as $coreClass) {
             require_once($coreClass);
         }
         
-        foreach (glob(GLOBAL_PATH.'platform/classes/*', GLOB_ONLYDIR) as $coreDirectory) {
-            $this->moduleDirectories[] = $coreDirectory;
+        //initialize all core module directories
+        foreach (glob(GLOBAL_PATH.'platform/classes/*', GLOB_ONLYDIR) as $moduleDirectory) {
+            $this->moduleDirectories[] = $moduleDirectory;
         }
-
+        
+        $this->projectDir     = GLOBAL_PATH.SITE_PATH.SITE_NAME.'/';
+        $this->controllersDir = $this->projectDir.'controllers/';
+        $this->viewsDir       = $this->projectDir.'views/';
+        
+        //initialize the database and memcache instances
         $this->dbName = $info['db']['select']['db'];
         $this->mc     = mc::getInstance($info['mc']);
         $this->db     = db::getInstance($info['db']);
 
-        $this->projectDir = GLOBAL_PATH.SITE_PATH.SITE_NAME.'/';
-        
+        //unset the db and mc variables from info and initialize the remaining varaibles as Core variables
         unset($info['db'], $info['mc']);
         
         foreach ($info as $key => $value) {
             $this->$key = $value;
         }
         
-        $this->controllersDir = $this->projectDir.'controllers/';
-        $this->viewsDir       = $this->projectDir.'views/';
-        
+        //if classes tree is provided, initialie the Core classesRequriementTree
         if (!empty($classesTree)) {
             foreach ($classesTree as $child => $parent) {
                 $this->classesRequriementTree[strtolower($child)] = strtolower($parent);
             } 
         }
-
-        return true;
     }
     
+    /**
+     * Requires a model with the specified class name and location and inits it as Core Model
+     * @param string $className - the name of the class (in lowercase); also the name of the file
+     * @param string $fileLocation - the path of the file to require
+     * @return object|false
+     */
     private function initCoreModel(string $className, string $fileLocation)
     {
         if (is_file($fileLocation)) {
@@ -169,6 +197,11 @@ class Core{
         return false;
     }
     
+    /**
+     * Requires a model from the site models
+     * @param string $className - the name of the class (in lowercase)
+     * @return object|false
+     */
     private function requireSiteSpecificModel(string $className)
     {
         return $this->initCoreModel(
@@ -177,7 +210,12 @@ class Core{
         );
     }
     
-    private function requireGlobalModel(string $className)
+    /**
+     * Requires a model from the project global models
+     * @param string $className - the name of the class (in lowercase)
+     * @return object|false
+     */
+    private function requireProjectGlobalModel(string $className)
     {
         return $this->initCoreModel(
             $className,
@@ -185,6 +223,11 @@ class Core{
         );
     }
     
+    /**
+     * Requires a model from the platform main models
+     * @param string $className - the name of the class (in lowercase)
+     * @return object|false
+     */
     private function requirePlatformModel(string $className)
     {
         return $this->initCoreModel(
@@ -193,9 +236,13 @@ class Core{
         );
     }
     
+    /**
+     * Requires a model from the platform modules
+     * @param string $className - the name of the class (in lowercase)
+     * @return object|false
+     */
     private function requireModuleModel(string $className)
     {
-        
         foreach ($this->moduleDirectories as $coreDirectory) {
             $model = $this->initCoreModel(
                 $className, $coreDirectory.'/'.$className.'.php'
@@ -207,7 +254,11 @@ class Core{
         return false;
     }
     
-    
+    /**
+     * Requires a model and inits is a Core variable
+     * @param string $className - the name of the class (in lowercase)
+     * @return object|false
+     */
     private function requireModel(string $className) 
     {
         $model = $this->requireSiteSpecificModel($className);
@@ -216,7 +267,7 @@ class Core{
         }
         unset($model);
         
-        $model = $this->requireGlobalModel($className);
+        $model = $this->requireProjectGlobalModel($className);
         if ($model !== false) {
             return $model;
         }
@@ -235,14 +286,22 @@ class Core{
         
         return false;
     }
-
-    public function __get($var)
+    
+    /**
+     * Gets a variable from Core;
+     * If the variable is available, it will return it
+     * If it's not it will attempt to require and initialize the model with the name of the variable
+     * It will return false if no model is found
+     * @param string $var - the name of the variable
+     * @return mixed
+     */
+    public function __get(string $varName)
     {
-        $var = strtolower($var);
+        $varName = strtolower($varName);
         
-        if (!isset($this->$var)) {
-            if (isset($this->classesRequriementTree[$var])) {
-                $parentName = $this->classesRequriementTree[$var];
+        if (!isset($this->$varName)) {
+            if (isset($this->classesRequriementTree[$varName])) {
+                $parentName = $this->classesRequriementTree[$varName];
                 
                 if (!isset($this->$parentName)) {
                     $this->requireModel($parentName); 
@@ -250,12 +309,12 @@ class Core{
                 
                 unset($parentName);
                 
-                return $this->requireModel($var);
+                return $this->requireModel($varName);
             }
             
-            return $this->requireModel($var);
+            return $this->requireModel($varName);
         } else {
-            return $this->$var;
+            return $this->$varName;
         }
         return false;
     }
@@ -345,9 +404,14 @@ class Core{
         }
     }
     
+    /**
+     * Checks if the client IP address is in the debug ips
+     * If the client has no IP address (is bot) or no debug ips are specified, he is considered developer
+     * @return bool 
+     */
     public function clientIsDeveoper()
     {
-        if (!isset($_SERVER['REMOTE_ADDR']) || empty($this->debugIps)) {
+        if ($this->isBot || !isset($_SERVER['REMOTE_ADDR']) || empty($this->debugIps)) {
             return true;
         }
         
@@ -366,6 +430,27 @@ class Core{
         if (!in_array($_SERVER['REMOTE_ADDR'], $this->debugIps)) {
             exit('This page is under development. We are sorry for the inconvenience. For questions please contact your developers.');
         }
+    }
+    
+    /**
+     * Returns true if the request is considered AJAX
+     */
+    public function requestIsAjax()
+    {
+        return $this->ajax;
+    }
+    
+    /**
+     * Set the request to be considered AJAX request or not
+     * @param bool $isAjax - if null or true, it will consider the request AJAX request
+     */
+    public function setAjaxRequest(bool $isAjax = null)
+    {
+        if ($isAjax === null) {
+            $isAjax = true;
+        }
+        
+        $this->ajax = $isAjax;
     }
 }
 ?>
