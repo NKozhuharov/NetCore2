545c6b307cc33e3708c3f0cb5612dfc1039c5f98
Nikola Kozhuharov <nikolakozhuharovv@gmail.com>
2019-03-28 10:05:51 -0400

    Changes in global variables; Classes tree moved to settings

--- core/core.php
+++ core/core.php
@@ -6,106 +6,106 @@ class Core{
      * An instance of the Database class
      */
     private $db;
-    
+
     /**
      * @var mc
      * An instance of the Memcache class
      */
     private $mc;
-    
+
     /**
      * @var string
      * The name of the database
      */
     private $dbName;
-    
+
     /**
      * @var array
      * Contains all the subfolders of the main classes folder
-     */   
+     */
     private $moduleDirectories = array();
-    
+
     /**
      * @var array
      * Contains the classes inheritance description
      */
     private $classesRequriementTree = array();
-    
+
     /**
      * @var string
      * The main directory with the project files
      */
-    private $projectDir;
-    
+    private $siteDir;
+
     /**
      * @var string
      * The directory with the controllers
      */
     private $controllersDir;
-    
+
     /**
      * @var string
      * The directory with the views
      */
     private $viewsDir;
-    
+
     /**
      * @var string
      * The timezone of the project
      * For a list of supported timezones - http://php.net/manual/en/timezones.php
      */
     private $timezone = 'Europe/Sofia';
-    
+
     /**
      * @var int
      * Query cache time; set to -1 for recache
      */
     private $cacheTime = 0;
-    
+
     /**
      * @var bool
      * Shows if the request is ajax
      * If it is, the platfornm won't include header and footer
      */
-    private $ajax = false; 
-    
+    private $ajax = false;
+
     /**
      * @var bool
      * Allows the platform to dump the controller variables in JSON format
      */
     private $json = false;
-    
+
     /**
     * @var array
     * Files that should be included before the current controller, array
-    * $Core->projectDir(like /var/www/sitename/admin/) will be concatenated in the start and .php on the end
-    */         
-    private $jsonAdditionalFiles = array(); 
-    
+    * $Core->siteDir(like /var/www/sitename/admin/) will be concatenated in the start and .php on the end
+    */
+    private $jsonAdditionalFiles = array();
+
     /**
      * @var string
      * The name of the global exception handler class
      */
     private $exceptionHandlerName = 'ExceptionHandler';
-    
+
     /**
      * @var object
      * An instance of the gloal exception handler class
      */
     private $exceptionhandler;
-    
+
     /**
      * @var string
      * The short name of the default language
      */
     private $defaultLanguage = 'bg';
-    
+
     /**
      * @var int
      * The id of the default language
      */
     private $defaultLanguageId = 24;
-    
+
     private $doNotStrip                = false;          //do not strip these parameters
     private $pageNotFoundLocation      = '/not-found';   //moust not be numeric so the rwrite can work
     private $allowFirstPage            = false;          //if allowed url like "/1" won't redirect to $pageNotFoundLocation
@@ -116,7 +116,7 @@ class Core{
     //rewrite override
     private $rewriteOverride           = array('' => 'index');
     //user model name
-    private $userModel                 = false;
+    private $userModel = null;
     //menu model name
     private $menuModel                 = false;
     //notifications(messages) model name
@@ -141,57 +141,56 @@ class Core{
         'SMTPSecure' => 'ssl',
         'Port'       => '465',
     );
-    
+
     /**
      * @var string
      * The text to show when unhandled exception occurs
      */
     private $generalErrorText = 'Sorry There has been some kind of an error with your request. If this persists please Contact Us.';
-    
+
     /**
      * @var array
      * A list of IPs, considered as developer IPs
      */
     private $debugIps = array();
-    
+
     /**
      * Creates an instance of the Core class
      * Throws Exception if info does not contain 'db' or 'mc'
      * @param array $info - the configuration array for the platfrom
-     * @param array $classesTree - the decsription of classes inheritane for the project (optional)
      * @throws Exception
      */
-    public function __construct(array $info, array $classesTree = null){
+    public function __construct(array $info){
         global $_DELETE, $_PUT, $_PATCH;
-        
+
         if (!isset($info['db']) || empty($info['db']) || !isset($info['mc']) || empty($info['mc'])) {
             throw new Exception ("'db' and 'mc' variables are required for a Core instance!");
         }
-        
+
         $this->ajax = (
             (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') ||
             (isset($_REQUEST['ajax'])) ||
             (isset($AJAX) && $AJAX == true)
         ) ? true : false;
-        
+
         $this->json = (isset($_REQUEST['json']) && !empty($_REQUEST['json'])) ? true : false;
-        
+
         //require base query builder
         $this->requireAllPHPFilesFromFolder(GLOBAL_PATH.'platform/basequery');
         //require base classes
         $this->requireAllPHPFilesFromFolder(GLOBAL_PATH.'platform/baseclasses');
         //require all Core classes
         $this->requireAllPHPFilesFromFolder(GLOBAL_PATH.'platform/classes');
-        
+
         //initialize all core module directories
         foreach (glob(GLOBAL_PATH.'platform/classes/*', GLOB_ONLYDIR) as $moduleDirectory) {
             $this->moduleDirectories[] = $moduleDirectory;
         }
-        
-        $this->projectDir     = GLOBAL_PATH.SITE_PATH.SITE_NAME.'/';
-        $this->controllersDir = $this->projectDir.'controllers/';
-        $this->viewsDir       = $this->projectDir.'views/';
-        
+
+        $this->siteDir     = GLOBAL_PATH.PROJECT_PATH.SITE_PATH.'/';
+        $this->controllersDir = $this->siteDir.'controllers/';
+        $this->viewsDir       = $this->siteDir.'views/';
+
         //initialize the database and memcache instances
         $this->dbName = $info['db']['select']['db'];
         $this->mc     = mc::getInstance($info['mc']);
@@ -199,18 +198,19 @@ class Core{
 
         //unset the db and mc variables from info and initialize the remaining varaibles as Core variables
         unset($info['db'], $info['mc']);
-        
+
         foreach ($info as $key => $value) {
             $this->$key = $value;
         }
-        
+
         //if classes tree is provided, initialie the Core classesRequriementTree
-        if (!empty($classesTree)) {
-            foreach ($classesTree as $child => $parent) {
+        if (!empty($this->classesRequriementTree)) {
+            foreach ($this->classesRequriementTree as $child => $parent) {
                 $this->classesRequriementTree[strtolower($child)] = strtolower($parent);
-            } 
+                unset($this->classesRequriementTree[$child]);
+            }
         }
-        
+
         date_default_timezone_set($this->timezone);
         //set locale for date functions
 
@@ -222,11 +222,11 @@ class Core{
         $this->GlobalFunctions->stripTagsOfArray($_PATCH, $this->doNotStrip);
 
         //WARNING: REWRITE NGNIX IN ORDER FOR IMAGES AND FILES TO WORK
-        $this->imagesStorage = GLOBAL_PATH.SITE_PATH.$this->imagesStorage;
-        $this->imagesDir     = GLOBAL_PATH.SITE_PATH.$this->imagesDir;
-        $this->filesDir      = GLOBAL_PATH.SITE_PATH.$this->filesDir;
+        $this->imagesStorage = GLOBAL_PATH.PROJECT_PATH.$this->imagesStorage;
+        $this->imagesDir     = GLOBAL_PATH.PROJECT_PATH.$this->imagesDir;
+        $this->filesDir      = GLOBAL_PATH.PROJECT_PATH.$this->filesDir;
     }
-    
+
     /**
      * Requires all PHP files from the provided directory
      * @param string $path - the path to search for files
@@ -237,7 +237,7 @@ class Core{
             require_once($class);
         }
     }
-    
+
     /**
      * Requires a model with the specified class name and location and inits it as Core Model
      * @param string $className - the name of the class (in lowercase); also the name of the file
@@ -253,7 +253,7 @@ class Core{
         }
         return false;
     }
-    
+
     /**
      * Requires a model from the site models
      * @param string $className - the name of the class (in lowercase)
@@ -263,10 +263,10 @@ class Core{
     {
         return $this->initCoreModel(
             $className,
-            $this->projectDir.'models/'.$className.'.php'
+            $this->siteDir.'models/'.$className.'.php'
         );
     }
-    
+
     /**
      * Requires a model from the project global models
      * @param string $className - the name of the class (in lowercase)
@@ -276,10 +276,10 @@ class Core{
     {
         return $this->initCoreModel(
             $className,
-            GLOBAL_PATH.SITE_PATH.'classes/'.$className.'.php'
+            GLOBAL_PATH.PROJECT_PATH.'classes/'.$className.'.php'
         );
     }
-    
+
     /**
      * Requires a model from the platform main models
      * @param string $className - the name of the class (in lowercase)
@@ -292,7 +292,7 @@ class Core{
             GLOBAL_PATH.'platform/classes/'.$className.'.php'
         );
     }
-    
+
     /**
      * Requires a model from the platform modules
      * @param string $className - the name of the class (in lowercase)
@@ -310,32 +310,32 @@ class Core{
         }
         return false;
     }
-    
+
     /**
      * Requires a model and inits is a Core variable
      * @param string $className - the name of the class (in lowercase)
      * @return object|false
      */
-    private function requireModel(string $className) 
+    private function requireModel(string $className)
     {
         $model = $this->requireSiteSpecificModel($className);
         if ($model !== false) {
             return $model;
         }
         unset($model);
-        
+
         $model = $this->requireProjectGlobalModel($className);
         if ($model !== false) {
             return $model;
         }
         unset($model);
-        
+
         $model = $this->requirePlatformModel($className);
         if ($model !== false) {
             return $model;
         }
         unset($model);
-        
+
         $model = $this->requireModuleModel($className);
         if ($model !== false) {
             return $model;
@@ -343,7 +343,7 @@ class Core{
         
         return false;
     }
-    
+
     /**
      * Gets a variable from Core;
      * If the variable is available, it will return it
@@ -355,39 +355,40 @@ class Core{
     public function __get(string $varName)
     {
         if (isset($this->$varName)) {
-            return $this->$varName;    
+            return $this->$varName;
         }
-        
+
         $varName = strtolower($varName);
 
         if ($varName === 'exceptionhandler') {
+
             if (empty($this->exceptionhandler)) {
                 return $this->requireModel(strtolower($this->exceptionHandlerName));
             } else {
                 return $this->exceptionhandler;
             }
         }
-        
+
         if (!isset($this->$varName)) {
             if (isset($this->classesRequriementTree[$varName])) {
                 $parentName = $this->classesRequriementTree[$varName];
-                
+
                 if (!isset($this->$parentName)) {
-                    $this->requireModel($parentName); 
+                    $this->requireModel($parentName);
                 }
-                
+
                 unset($parentName);
-                
+
                 return $this->requireModel($varName);
             }
-            
+
             return $this->requireModel($varName);
         } else {
             return $this->$varName;
         }
         return false;
     }
-    
+
     /**
      * Redirects to the specifed URL
      * If it's an AJAX request, it will redirect with javascript
@@ -404,7 +405,7 @@ class Core{
 
         return true;
     }
-    
+
     /**
      * Check a condition; if it's false, it shows page not found
      * @param bool $check - the condition
@@ -418,7 +419,7 @@ class Core{
             die;
         }
     }
-    
+
     /**
      * Shows a variable dump
      * If the debugIps is not empty, it will show ouput only for them
@@ -448,25 +449,25 @@ class Core{
             }
         }
     }
-    
+
     /**
      * Checks if the client IP address is in the debug ips
      * If the client has no IP address (is bot) or no debug ips are specified, he is considered developer
-     * @return bool 
+     * @return bool
      */
     public function clientIsDeveoper()
     {
         if ($this->isBot || !isset($_SERVER['REMOTE_ADDR']) || empty($this->debugIps)) {
             return true;
         }
-        
+
         if (in_array($_SERVER['REMOTE_ADDR'], $this->debugIps)) {
             return true;
         }
-        
+
         return false;
     }
-    
+
     /**
      * Locks the page for all users, which are not in the debugIps
      */
@@ -476,7 +477,7 @@ class Core{
             exit('This page is under development. We are sorry for the inconvenience. For questions please contact your developers.');
         }
     }
-    
+
     /**
      * Returns true if the request is considered AJAX
      */
@@ -484,7 +485,7 @@ class Core{
     {
         return $this->ajax;
     }
-    
+
     /**
      * Set the request to be considered AJAX request or not
      * @param bool $isAjax - if null or true, it will consider the request AJAX request
@@ -494,10 +495,10 @@ class Core{
         if ($isAjax === null) {
             $isAjax = true;
         }
-        
+
         $this->ajax = $isAjax;
     }
-    
+
     /**
      * Set a new items per page count, must be bigger than 0
      * @param int $itemsPerPage - the new items per page
@@ -508,7 +509,7 @@ class Core{
         if ($itemsPerPage <= 0) {
             throw new Exeption("Items per page must be bigger than 0");
         }
-        
+
         $this->itemsPerPage = $itemsPerPage;
     }
 }
