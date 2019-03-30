063298a6afb5b4df33a11ef6962889e9fa9fd8fc
Nikola Kozhuharov <nikolakozhuharovv@gmail.com>
2019-03-30 11:21:22 -0400

    Added addOrderBy function of the BaseOrderBy trait

--- basequery/_traitBaseJoin.php
+++ basequery/_traitBaseJoin.php
@@ -1,32 +1,32 @@
 <?php
-    class BaseJoinData 
+    class BaseJoinData
     {
         /**
          * @var string
          * Join type
          */
         public $type;
-        
+
         /**
          * The name of the table to join
          */
         public $tableToJoin;
-        
+
         /**
          * The name of the field from the table to join
          */
         public $tableField;
-        
+
         /**
          * The name of the field from the parent table to join to
          */
         public $parentField;
-        
+
         /**
          * The name of the parent table to join to
          */
         public $parentTable;
-        
+
         /**
          * Creates a new instance of the BaseJoinData class
          * @param string $type - the join type
@@ -40,27 +40,27 @@
             if (empty($tableToJoin)) {
                 throw new Exception("Provide the name of the table to join");
             }
-            
+
             if (empty($tableField)) {
                 throw new Expceiotn("Provide the names of the field from the joining table");
             }
-            
+
             if (empty($parentField)) {
                 throw new Expceiotn("Provide the names of the field from the parent table");
             }
-            
+
             if (empty($parentTable)) {
                 throw new Exception("Provide the name of the parent table to join");
             }
-            
+
             $this->type = $type;
             $this->tableToJoin = $tableToJoin;
-            $this->tableField = $tableField;
+            $this->tableField  = $tableField;
             $this->parentField = $parentField;
             $this->parentTable = $parentTable;
         }
     }
-    
+
     trait BaseJoin
     {
         /**
@@ -68,10 +68,10 @@
          * The joins to the table
          */
         private $joins;
-        
+
         /**
          * Adds a INNER JOIN to the query collection of joins
-         * @param string $tableToJoin - the name of the table to join 
+         * @param string $tableToJoin - the name of the table to join
          * @param string $tableField  - the name of the field from the table to join
          * @param string $parentField - the name of the field from the parent table to join to
          * @param string $parentTable - the name of the parent table to join to (optional)
@@ -81,13 +81,13 @@
             if (empty($parentTable)) {
                 $parentTable = $this->tableName;
             }
-            
+
             $this->joins[] = new BaseJoinData("INNER JOIN", $tableToJoin, $tableField, $parentField, $parentTable);
         }
-        
+
         /**
          * Adds a OUTER JOIN to the query collection of joins
-         * @param string $tableToJoin - the name of the table to join 
+         * @param string $tableToJoin - the name of the table to join
          * @param string $tableField  - the name of the field from the table to join
          * @param string $parentField - the name of the field from the parent table to join to
          * @param string $parentTable - the name of the parent table to join to (optional)
@@ -97,13 +97,13 @@
             if (empty($parentTable)) {
                 $parentTable = $this->tableName;
             }
-            
+
             $this->joins[] = new BaseJoinData("OUTER JOIN", $tableToJoin, $tableField, $parentField, $parentTable);
         }
-        
+
         /**
          * Adds a LEFT JOIN to the query collection of joins
-         * @param string $tableToJoin - the name of the table to join 
+         * @param string $tableToJoin - the name of the table to join
          * @param string $tableField  - the name of the field from the table to join
          * @param string $parentField - the name of the field from the parent table to join to
          * @param string $parentTable - the name of the parent table to join to (optional)
@@ -113,13 +113,13 @@
             if (empty($parentTable)) {
                 $parentTable = $this->tableName;
             }
-            
+
             $this->joins[] = new BaseJoinData("LEFT JOIN", $tableToJoin, $tableField, $parentField, $parentTable);
         }
-        
+
         /**
          * Adds a RIGHT JOIN to the query collection of joins
-         * @param string $tableToJoin - the name of the table to join 
+         * @param string $tableToJoin - the name of the table to join
          * @param string $tableField  - the name of the field from the table to join
          * @param string $parentField - the name of the field from the parent table to join to
          * @param string $parentTable - the name of the parent table to join to (optional)
@@ -129,10 +129,10 @@
             if (empty($parentTable)) {
                 $parentTable = $this->tableName;
             }
-            
+
             $this->joins[] = new BaseJoinData("RIGHT JOIN", $tableToJoin, $tableField, $parentField, $parentTable);
         }
-        
+
         /**
          * Adds all joins to the query
          */
--- basequery/_traitBaseOrderBy.php
+++ basequery/_traitBaseOrderBy.php
@@ -9,17 +9,47 @@
 
         /**
          * Sets the ORDER BY clause of the query
-         * It must contain either ASC or DESC (case sensitive)
-         * @param string $where - the body of the order by clause
+         * Thorws Exception if the order does not contain ASC or DESC (case sensitive)
+         * @param string $orderBy - the body of the order by clause
          * @throws Exception
          */
         public function setOrderBy(string $orderBy)
         {
             if (!strstr($orderBy, self::ORDER_ASC) && !strstr($orderBy, self::ORDER_DESC)) {
-                throw new Exception("Invalid order provided! Order must contain ASC or DESC!");
+                throw new Exception("Invalid order type provided, it must contain ASC or DESC");
             }
-
+            
             $this->orderBy = $orderBy;
+        }
+        
+        /**
+         * Adds an ORDER BY clause to the query
+         * It can be called multiple times, to add additional orders
+         * Thorws Exception if the field or the order type are empty
+         * Thorws Exception if the order type is not ASC or DESC (case sensitive)
+         * @param string $field - the field to order by
+         * @param string $orderType - the order type (ASC or DESC)
+         * @throws Exception
+         */
+        public function addOrderBy(string $field, string $orderType) 
+        {
+            if (empty($field)) {
+                throw new Exception("Select a field to order by");
+            }
+            
+            if (empty($orderType)) {
+                throw new Exception("Select order type");
+            }
+            
+            if ($orderType !== self::ORDER_ASC && $orderType !== self::ORDER_DESC) {
+                throw new Exception("Invalid order type provided, it must be ASC or DESC");
+            }
+            
+            if (!empty($this->orderBy)) {
+                $this->orderBy .= ", {$field} {$orderType}";
+            } else {
+                $this->orderBy = "{$field} {$orderType}";
+            }
         }
 
         /**
