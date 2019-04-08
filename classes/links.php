32abeb5a3e9ea6059bdf0486e3c5043b346bab1b
Nikola Kozhuharov <nikolakozhuharovv@gmail.com>
2019-04-08 07:07:53 -0400

    Fixed a bug in Links when showing page not found

--- classes/links.php
+++ classes/links.php
@@ -2,18 +2,18 @@
 class Links extends Base
 {
     const MULTI_LANGUAGE_LINKS_SEPARATOR = '^';
-    
+
     /**
      * @var array
      * Contains a map, between the multilanguage links and their according controllers and languages
      */
     private $multiLanguageLinksInfo;
-    
+
     /**
      * Creates a new instacne of the Links class
      * If the multiLanguageLinks Core variable is set to true, it will initialize the Base variables
      * WARNING: it requires the `multilanguage_links` table to work
-     * 
+     *
      *    CREATE TABLE `multilanguage_links` (
           `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
           `controller_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -28,14 +28,14 @@ class Links extends Base
     public function __construct()
     {
         global $Core;
-        
+
         if ($Core->multiLanguageLinks === true) {
             $this->tableName = 'multilanguage_links';
             $this->parentField = 'language_id';
             $this->getMultiLanguageLinks();
         }
     }
-    
+
     /**
      * Gets the map, between the multilanguage links and their according controllers and languages
      * from the `multilanguage_links` table and initializes the multiLanguageLinksInfo property
@@ -43,13 +43,13 @@ class Links extends Base
     private function getMultiLanguageLinks()
     {
         global $Core;
-        
+
         $Core->db->query("
-            SELECT 
+            SELECT
                 CONCAT(`controller_name`,'".self::MULTI_LANGUAGE_LINKS_SEPARATOR."',`language_id`) AS 'controller',
-                `path` 
-            FROM 
-                `{$Core->dbName}`.`{$this->tableName}`", 
+                `path`
+            FROM
+                `{$Core->dbName}`.`{$this->tableName}`",
             $this->queryCacheTime,
             'fillArraySingleField',
             $this->multiLanguageLinksInfo,
@@ -57,16 +57,16 @@ class Links extends Base
             'controller'
         );
     }
-    
+
     /**
      * Another option to get a link, according to the controller
      * @param string $controllerName - the name of the controller for the link
      */
-    public function __get(string $controllerName) 
+    public function __get(string $controllerName)
     {
         return $this->getLink($controllerName);
     }
-    
+
     /**
      * Gets a link, according to the provided controller.
      * Throws Exception if multi-language links are available and the controller does not exist
@@ -79,21 +79,25 @@ class Links extends Base
     public function getLink(string $controllerName, string $additional = null)
     {
         global $Core;
-        
+
         if ($Core->multiLanguageLinks === true) {
             $controllerName = $controllerName.self::MULTI_LANGUAGE_LINKS_SEPARATOR.$Core->Language->getCurrentLanguageId();
             $link = array_search($controllerName, $this->multiLanguageLinksInfo);
-            
+
             if (empty($link)) {
-                throw new Exception ("The following controller was not found in the {$this->tableName} table - $controllerName");
+                $controllerName = explode(self::MULTI_LANGUAGE_LINKS_SEPARATOR, $controllerName);
+                throw new Exception (
+                    "The following controller was not found in the `{$this->tableName}` table - ".
+                    "{$controllerName[0]} for language id {$controllerName[1]}"
+                );
             }
-            
+
             return "/{$link}{$additional}";
         }
-        
+
         return "/{$controllerName}{$additional}";
     }
-    
+
     /**
      * Gets the link controller (and language from the `multilanguage_links` table, if multi-language links are available)
      * It will redirect to '/not-found' if a controller is not found in the `multilanguage_links` table
@@ -103,27 +107,27 @@ class Links extends Base
     public function parseLink(string $path)
     {
         global $Core;
-        
+
         if (isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] === '/') {
             return $path;
         }
-        
+
         if ($Core->multiLanguageLinks === true) {
             foreach ($this->multiLanguageLinksInfo as $pathTranslation => $controller) {
                 if ($pathTranslation === substr($path, 0, strlen($pathTranslation))) {
                     $controller = explode(self::MULTI_LANGUAGE_LINKS_SEPARATOR, $controller);
-                    
+
                     if ($controller[1] != $Core->Language->getCurrentLanguageId()) {
                         $Core->Language->changeLanguageById($controller[1]);
                     }
-                    
+
                     return $controller[0];
                 }
             }
             
-            $Core->doOrDie();
+            return $Core->pageNotFoundLocation;
         }
-        
+
         return $path;
     }
 }
