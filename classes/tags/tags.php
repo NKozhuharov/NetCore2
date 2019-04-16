f0a84ca0f16b3a73d5c3bf58ffe0dba94b4c3947
Nikola Kozhuharov <nikolakozhuharovv@gmail.com>
2019-04-16 08:45:51 -0400

    Complete the development of the Tags module

--- classes/tags/tags.php
+++ classes/tags/tags.php
@@ -3,31 +3,75 @@ class Tags extends Base
 {
     use InputAndTranslate;
     
+    /**
+     * Creates an instance of Tags class
+     */
     public function __construct()
     {
         $this->tableName = 'tags';
         $this->orderByField = 'id';
         $this->orderByType = self::ORDER_DESC;
         $this->translationFields = array('name');
+        $this->turnOffTranslation();
     }
     
+    /**
+     * Override the hook, to add the translations of all tags for all languages
+     * @param BaseSelect $selector - the selector from getAll
+     * @return BaseSelect
+     */
+    public function getAllSelectHook(BaseSelect $selector)
+    {
+        global $Core;
+        
+        $activeLanguageIds = $Core->Language->getActiveLanguagesIds();
+        
+        $selector->addField('id', 'id', $this->tableName);
+        $selector->addField('name', 'name', $this->tableName);
+        foreach ($activeLanguageIds as $languageId) {
+            if ($languageId !== $Core->Language->getDefaultLanguageId()) {
+                $selector->addField('name', "name_{$languageId}", "lang_{$languageId}");
+                $selector->addLeftJoin(
+                    "{$this->tableName}_lang", 
+                    'object_id', 
+                    'id', 
+                    null, 
+                    "lang_{$languageId}",
+                    "AND `{$Core->dbName}`.`lang_{$languageId}`.`lang_id` = {$languageId}"
+                );
+            }
+        }
+        
+        return $selector;
+    }
     
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+    /**
+     * Override the hook, to add the translations of the tag for all languages
+     * @param BaseSelect $selector - the selector from getById
+     * @return BaseSelect
+     */
+    public function getByIdSelectHook(BaseSelect $selector)
+    {
+        global $Core;
+        
+        $activeLanguageIds = $Core->Language->getActiveLanguagesIds();
+        
+        $selector->addField('id', 'id', $this->tableName);
+        $selector->addField('name', 'name', $this->tableName);
+        foreach ($activeLanguageIds as $languageId) {
+            if ($languageId !== $Core->Language->getDefaultLanguageId()) {
+                $selector->addField('name', "name_{$languageId}", "lang_{$languageId}");
+                $selector->addLeftJoin(
+                    "{$this->tableName}_lang", 
+                    'object_id', 
+                    'id', 
+                    null, 
+                    "lang_{$languageId}",
+                    "AND `{$Core->dbName}`.`lang_{$languageId}`.`lang_id` = {$languageId}"
+                );
+            }
+        }
+        
+        return $selector;
+    }
 }
