a431f8e629c573398dea6fa3fb00f9cb6f63c9c0
Nikola Kozhuharov <nikolakozhuharovv@gmail.com>
2019-06-06 07:59:59 -0400

    Improved creations of project and site

--- deploy/createproject.sh
+++ deploy/createproject.sh
@@ -18,12 +18,12 @@ BLUE="\033[0;36m"
 RED="\033[0;31m"
 NC="\033[0m" # No Color
 
-mkdir "$FPREFIX/${1}/"
-mkdir "$FPREFIX/${1}/classes/"
-mkdir "$FPREFIX/${1}/files/"
-mkdir "$FPREFIX/${1}/images/"
-mkdir "$FPREFIX/${1}/images_org/"
-mkdir "$FPREFIX/${1}/settings/"
+mkdir -p "$FPREFIX/${1}/"
+mkdir -p "$FPREFIX/${1}/classes/"
+mkdir -p "$FPREFIX/${1}/files/"
+mkdir -p "$FPREFIX/${1}/images/"
+mkdir -p "$FPREFIX/${1}/images_org/"
+mkdir -p "$FPREFIX/${1}/settings/"
 
 chown -R apache:apache "$FPREFIX/${1}/files/"
 chown -R apache:apache "$FPREFIX/${1}/images/"
@@ -31,11 +31,20 @@ chown -R apache:apache "$FPREFIX/${1}/images_org/"
 
 printf "Created project ${GREEN}${1}${NC}\n"
 
-mkdir "$FPREFIX/${1}/${2}/"
-mkdir "$FPREFIX/${1}/${2}/www"
+mkdir -p "$FPREFIX/${1}/${2}/"
+mkdir -p "$FPREFIX/${1}/${2}/controllers"
+mkdir -p "$FPREFIX/${1}/${2}/includes"
+mkdir -p "$FPREFIX/${1}/${2}/models"
+mkdir -p "$FPREFIX/${1}/${2}/parts"
+mkdir -p "$FPREFIX/${1}/${2}/templates"
+mkdir -p "$FPREFIX/${1}/${2}/views"
+mkdir -p "$FPREFIX/${1}/${2}/www"
 
 cp "$FPREFIX/platform/deploy/exampleindex.php" "$FPREFIX/${1}/${2}/www/index.php"
 cp "$FPREFIX/platform/deploy/examplesettings.php" "$FPREFIX/${1}/settings/${2}.php"
+
+touch "$FPREFIX/${1}/${2}/includes/header.php"
+touch "$FPREFIX/${1}/${2}/includes/footer.php"
 
 printf "Created site ${BLUE}${2}${NC}\n"
 printf "${RED}Please have a look at the settings folder to complete the setup of the site!${NC}\n"
