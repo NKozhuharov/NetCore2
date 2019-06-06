3f810f4c9675e3e44e375a6aab102bc7c500a587
Nikola Kozhuharov <nikolakozhuharovv@gmail.com>
2019-06-06 07:38:14 -0400

    Added an option to deploy a site for a project of the platform using shell script

---
+++ deploy/createsite.sh
@@ -0,0 +1,31 @@
+#Param 1 is project name
+#Param 2 is site name
+
+if [ -z "${1}" ] 
+then
+    exit "Define a project name!"
+fi
+
+if [ -z "${2}" ] 
+then
+    exit "Define a site name!"
+fi
+
+FPREFIX="/var/www"
+
+GREEN="\033[0;32m"
+BLUE="\033[0;36m"
+RED="\033[0;31m"
+NC="\033[0m" # No Color
+
+mkdir "$FPREFIX/${1}/${2}/"
+mkdir "$FPREFIX/${1}/${2}/www"
+
+cp "$FPREFIX/platform/deploy/exampleindex.php" "$FPREFIX/${1}/${2}/www/index.php"
+cp "$FPREFIX/platform/deploy/examplesettings.php" "$FPREFIX/${1}/settings/${2}.php"
+
+printf "Created site ${BLUE}${2}${NC}\n"
+printf "${RED}Please have a look at the settings folder to complete the setup of the site!${NC}\n"
+printf "${RED}Please have a look at the index in www folder and setup SITE_PATH and PROJECT_PATH variables!${NC}\n"
+
+printf "Finished!\n"
